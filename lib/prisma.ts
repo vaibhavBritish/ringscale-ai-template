dce49c2c9abafea2b2e0ca05211eafd75b17c292
vaibhav <vaibhav@vaibhavs-MacBook-Pro.local>
2026-03-17 16:11:35 +0530

    fixed prisma error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -8,13 +8,11 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is not set");
 }
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClient;
 };
 
-const adapter = new PrismaPg({
-  connectionString,
-});
+const adapter = new PrismaPg({ connectionString });
 
 export const prisma =
   globalForPrisma.prisma ??
